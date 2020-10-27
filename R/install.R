feb091916094dfed8322017d8186ae594a5bd83f
t03jam8 <grovejam@gmail.com>
2020-10-27 17:28:29 +0100

    Add packages to yaml

--- R/install.R
+++ R/install.R
@@ -1,13 +1,21 @@
-install <- function(pkg){
-  
+install <- function(pkg) {
   pkg_dir <- paste0(getwd(), "/r_packages")
-  
-  while(!dir.exists('r_packages')){
+
+  while (!dir.exists("r_packages")) {
     # Create r_packages dir
     if (!dir.exists((pkg_dir))) {
       dir.create(pkg_dir)
     }
   }
-  
+
   install.packages(pkg, lib = pkg_dir)
+
+  pakages <- yaml::read_yaml(file = "./packages.yaml")
+  if (!package$depencies) {
+    packages$depenencies <- c(
+      list(pkg = packageVersion(pkg))
+    )
+  } else {
+    append(packages$depenencies, list(pkg = packageVersion(pkg)))
+  }
 }
